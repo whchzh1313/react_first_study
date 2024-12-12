2c4463ee4c6b4a004279e52d6f8d73c06cac938a
whchzh1313@gmail.com <whchzh1313@gmail.com>
2024-12-12 16:57:10 +0900

    app.tsx에 라우터 추가

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>about 페이지 입니다.</div>
+  )
+}
+
+export default about
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>메인 홈페이지 파일입니다.</div>
+  )
+}
+
+export default index
