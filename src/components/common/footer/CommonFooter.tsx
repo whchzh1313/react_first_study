11cdd61d2917a29dd0fc209160461cd2182a78c4
whchzh1313@gmail.com <whchzh1313@gmail.com>
2024-12-13 00:44:09 +0900

    (14406) 더미 이미지 카드 생성

--- src/components/common/footer/CommonFooter.tsx
+++ src/components/common/footer/CommonFooter.tsx
@@ -5,12 +5,12 @@ function CommonFooter() {
     <div className={styles.footer}>
         <div className={styles.pagination}>
             <button className={styles.pagination__button}>
-                <img src="/assets/icons/arrow-left.svg" alt="" />
+                <img src="/src/assets/icons/icon-arrowLeft.svg" alt="" />
             </button>
             {/* 변경될 UI 부분 */}
             <span>1</span>
             <button className={styles.pagination__button}>
-                <img src="/assets/icons/arrow-left.svg" alt="" />
+                <img src="/src/assets/icons/icon-arrowRight.svg" alt="" />
             </button>
         </div>
     </div>
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,6 +2,7 @@ import CommonHeader from '@/components/common/header/CommonHeader'
 import CommonSearchBar from '@/components/common/searchBar/CommonSearchBar'
 import CommonNav from '@/components/common/navigation/CommonNav'
 import CommonFooter from '@/components/common/footer/CommonFooter'
+import Card from './components/Card'
 
 // CSS
 import styles from './styles/index.module.scss'
@@ -22,7 +23,13 @@ function index() {
             <CommonSearchBar />
           </div>
         </div>
-        <div className={styles.page__contents__imageBox}></div>
+        <div className={styles.page__contents__imageBox}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
       {/* 공통 푸터 UI 부분 */}
       <CommonFooter />
